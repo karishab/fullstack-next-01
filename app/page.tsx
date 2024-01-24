00e5d6d15d125413cbac169f822c490d2465b982
Salar Shabahang <karishab86@gmail.com>
2024-01-24 10:09:15 +0100

    env clerk sign-in-up

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 export default function Home() {
-    return (
-      <div>Home</div>
+  return (
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">Das Best Developer App.</h1>
+        <p className="text-2xl text-white/60 mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit, voluptas.</p>
+        <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">Get Started</button>
+      </div>
+    </div>
   )
 }
