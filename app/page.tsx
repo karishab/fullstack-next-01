14ff510a2019810067cc2afe7fe3b8459dac7b55
Salar Shabahang <karishab86@gmail.com>
2024-01-25 11:19:40 +0100

    butttun link href zufügen

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import Link from 'next/link'
 export default function Home() {
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
       <div className="w-full max-w-[600px] mx-auto">
         <h1 className="text-6xl mb-4">Das Best Developer App.</h1>
         <p className="text-2xl text-white/60 mb-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit, voluptas.</p>
-        <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">Get Started</button>
+        <Link href="/developer">
+        <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
+          Get Started
+          </button>
+        </Link>
       </div>
     </div>
   )
