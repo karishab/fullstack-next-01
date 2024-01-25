d568ecfcde114110d3d902ff607d2cb09a1e3311
Salar Shabahang <karishab86@gmail.com>
2024-01-25 11:27:12 +0100

    signUp aftersignupurl

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,9 @@
 import { SignUp } from '@clerk/nextjs'
+import { RedirectUrl } from '@clerk/nextjs/server';
+import { redirect } from 'next/dist/server/api-utils';
 
 const SignUpPage = () => {
-    return <SignUp />
+    return <SignUp  afterSignUpUrl="/new-user" redirectUrl="/"/>
 }
 
 export default SignUpPage;
